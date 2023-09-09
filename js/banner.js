aa588f47525be47ff050dea7ccc1bf0993a0c12b
iammithani <muhammadusmanmithani@gmail.com>
2023-09-09 15:27:16 +0500

    added assets, styling and responsiveness. added js

--- js/banner.js
+++ js/banner.js
@@ -1,24 +1,30 @@
-let randomTimer = (min, max) => {
+let randomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 let string = "A Computer for You...";
-let spanInOut = [15,18]
 let array = string.split("");
-let motto = document.getElementsByClassName("motto")[0];
+let motto = document.getElementsByClassName("caption")[0];
+let audio = document.getElementsByClassName("audio")
+Array.from(audio).forEach((element) => {
+    element.volume = 0.3;
+});
 let load = async () => {
     for (let i=0;i<array.length;i++) {
-        if (i==spanInOut[0]) {
-            motto.innerHTML += "\<span\>";
+        let audioIndex = randomInt(0,3);
+        if (audio[audioIndex].paused) {
+            audio[audioIndex].play().catch(() => {
+                console.info("Audio not played due to no user interaction with DOM!")
+            });
         }
-        if (i==spanInOut[1]) {
-            motto.innerHTML += "\<\/span\>";
-        }
-        await sleep(randomTimer(150,200))
+        await sleep(randomInt(250,300));
         motto.innerHTML += array[i];
-        console.log(i);
     }
+    motto.innerHTML = "A Computer for <span class='colored white'>You</span>...";
+    sleep(50).then(() => {
+        document.getElementsByClassName("colored")[0].classList.remove("white");
+    });
 }
 load()
