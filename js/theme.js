52efdb12adab4c9936016597c8a5dba5bc5b226d
iammithani <muhammadusmanmithani@gmail.com>
2023-09-10 00:20:20 +0500

    default theme detector + eye-pleasing changes

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,8 @@
 // make a theme switcher and save the theme to local storage
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !localStorage.getItem("theme")) {
+    localStorage.setItem("theme", "dark");
+    console.log("System default dark theme is enabled by default! Enabling dark mode...");
+}
 let themeSwitcher = document.getElementsByClassName("theme-switcher")[0];
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
